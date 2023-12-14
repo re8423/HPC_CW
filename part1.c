2b465624bbac1af0bc1ddb1e6e029ad426814d72
re8423 <maina8423@gmail.com>
2023-12-14 22:37:41 +0000

    bash

--- part1.c
+++ part1.c
@@ -1,10 +1,14 @@
 #include <stdio.h>				// needed for printing
 #include <math.h>				// needed for tanh, used in init function
 #include "params.h"				// model & simulation parameters
+#include <omp.h> //openmp header file
 
 void init(double u[N][N], double v[N][N]){
 	double uhi, ulo, vhi, vlo;
 	uhi = 0.5; ulo = -0.5; vhi = 0.1; vlo = -0.1;
+	omp_set_num_threads(128);
+
+	#pragma omp parrallel for
 	for (int i=0; i < N; i++){
 		for (int j=0; j < N; j++){
 			u[i][j] = ulo + (uhi-ulo)*0.5*(1.0 + tanh((i-N/2)/16.0));
