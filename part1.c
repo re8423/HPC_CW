7877a39f007b339fb369bf3203d7f19f617cb4c8
re8423 <maina8423@gmail.com>
2023-12-16 09:11:52 +0000

    bash

--- part1.c
+++ part1.c
@@ -67,9 +67,10 @@ void dxdt(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){ // 
 }
 
 void step(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){
-	#pragma omp parrallel
-	{
-		#pragma omp for  //64 seems to work best, why?
+	// #pragma omp parrallel
+	// {
+	// 	#pragma omp for schedule(static, 64) //64 seems to work best, why?
+			#pragma omp parrallel for
 			for (int i = 0; i < N/4; i++){
 				for (int j = 0; j < N/4; j++){
 					u[i*4][j*4] += dt*du[i*4][j*4];
@@ -87,7 +88,7 @@ void step(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){
 				}
 			}
 	}
-}
+// }
 
 double norm(double x[N][N]){
 	double nrmx = 0.0;
