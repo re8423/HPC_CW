f5346dde6e51c07abadfd322cddcc7e28cd4da9b
re8423 <maina8423@gmail.com>
2023-12-16 03:56:50 +0000

    bash

--- part1.c
+++ part1.c
@@ -26,49 +26,37 @@ void dxdt(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){ // 
 		#pragma omp for
 			for (i = 0; i < N; i++){
 				for (j = 0; j < N; j++){
-					#pragma omp task 
-					{
-						#pragma omp task default(shared) shared(up, down, left, right)
-						{
-							if (i == 0){
-								down = i;
-							}
-							else{
-								down = i-1;
-							}
-
-							if (i == N-1){
-								up = i;
-							}
-							else{
-								up = i+1;
-							}
-
-							if (j == 0){
-								left = j;
-							}
-							else{
-								left = j-1;
-							}
-
-							if (j == N-1){
-								right = j;
-							}
-							else{
-								right = j+1;
-							}
-						}
-					
-
-						lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
-						lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
-						du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
-						dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);
-					
-						
+					if (i == 0){
+						down = i;
+					}
+					else{
+						down = i-1;
 					}
 
-	
+					if (i == N-1){
+						up = i;
+					}
+					else{
+						up = i+1;
+					}
+
+					if (j == 0){
+						left = j;
+					}
+					else{
+						left = j-1;
+					}
+
+					if (j == N-1){
+						right = j;
+					}
+					else{
+						right = j+1;
+					}
+				lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
+				lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
+				du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
+				dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);
 				}
 			}
 			// #pragma omp taskwait
