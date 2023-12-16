bf0425d1095e69c6375438e0b52f044beb77a8a0
re8423 <maina8423@gmail.com>
2023-12-16 04:27:38 +0000

    bash

--- part1.c
+++ part1.c
@@ -90,21 +90,19 @@ double norm(double x[N][N]){
 	double nrmx = 0.0;
 	// #pragma omp parrallel for collapse(2)
 
-
 	#pragma omp parrallel for reduction(+:nrmx)
-		for (int i = 0; i < N; i++){
-			for (int j = 0; j < N; j++){
-				nrmx += x[i][j]*x[i][j];
-			}
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			nrmx += x[i][j]*x[i][j];
 		}
+	}
 
-	
 	return nrmx;
 }
 
 int main(int argc, char** argv){
 	
-	omp_set_num_threads(128);
+	omp_set_num_threads(2);
 
 	double t = 0.0, nrmu, nrmv;
 	double u[N][N], v[N][N], du[N][N], dv[N][N];
