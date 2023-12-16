9dedf1a6cef4e0936e2bee5ea5bc19e25b6fbd2e
re8423 <maina8423@gmail.com>
2023-12-16 05:06:34 +0000

    bash

--- part1.c
+++ part1.c
@@ -24,61 +24,44 @@ void init(double u[N][N], double v[N][N]){
 void dxdt(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){ // u,v are not being changed (no need for reduction)
 	double lapu, lapv;
 	int up, down, left, right;
-	int i, j;
 	// #pragma omp parrallel for schedule(static, 128)
 
 	#pragma omp parrallel
 	{
-		#pragma omp for schedule(dynamic)
-			for (i = 0; i < N; i++){
-				for (j = 0; j < N; j++){
-					#pragma omp task default(shared)
-					{
-						#pragma omp task default(shared)
-						{
-							if (i == 0){
-								down = i;
-							}
-							else{
-								down = i-1;
-							}
-						}
-						#pragma omp task default(shared)
-						{
-							if (i == N-1){
-								up = i;
-							}
-							else{
-								up = i+1;
-							}
-						}
-						#pragma omp task default(shared)
-						{
-							if (j == 0){
-								left = j;
-							}
-							else{
-								left = j-1;
-							}
-						} 
-						#pragma omp task default(shared)
-						{
-							if (j == N-1){
-								right = j;
-							}
-							else{
-								right = j+1;
-							}
-						}
-						#pragma omp taskwait
+		#pragma omp for schedule(dynamic)  //simple if would be faster than overhead of spawning a task (i.e. faster for same thread to do it)
+			for (int i = 0; i < N; i++){
+				for (int j = 0; j < N; j++){
+					if (i == 0){
+						down = i;
 					}
-					#pragma omp task default(shared)
-					{
-						lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
-						lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
-						du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
-						dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);
+					else{
+						down = i-1;
 					}
+
+					if (i == N-1){
+						up = i;
+					}
+					else{
+						up = i+1;
+					}
+
+					if (j == 0){
+						left = j;
+					}
+					else{
+						left = j-1;
+					}
+
+					if (j == N-1){
+						right = j;
+					}
+					else{
+						right = j+1;
+					}
+				lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
+				lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
+				du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
+				dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);
 				}
 			}		
 	}
