2057cdffab51e6547c7c591ef4dacc8a9a0f422c
re8423 <maina8423@gmail.com>
2023-12-16 10:16:01 +0000

    bash

--- part1.c
+++ part1.c
@@ -129,8 +129,8 @@ int main(int argc, char** argv){
 			// calculate the norms
 			nrmu = norm(u);
 			nrmv = norm(v);
-			// printf("t = %2.1f\tu-norm = %2.5f\tv-norm = %2.5f\n", t, nrmu, nrmv);
-			// fprintf(fptr, "%f\t%f\t%f\n", t, nrmu, nrmv);
+			printf("t = %2.1f\tu-norm = %2.5f\tv-norm = %2.5f\n", t, nrmu, nrmv);
+			fprintf(fptr, "%f\t%f\t%f\n", t, nrmu, nrmv);
 		}
 	}
 	
