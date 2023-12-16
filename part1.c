6f08caca11daa4663104dec63930551df36ddb9c
re8423 <maina8423@gmail.com>
2023-12-16 08:46:23 +0000

    bash

--- part1.c
+++ part1.c
@@ -31,38 +31,163 @@ void dxdt(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){ // 
 	#pragma omp parrallel
 	{ //Each value in grid is diff, hence computing du, dv for each i,v would be different, hence dynamic (i.e. work stealing is better)
 		#pragma omp for schedule(dynamic, 64)  //64 + simple if would be faster than overhead of spawning a task (i.e. faster for same thread to do it)
-			for (int i = 0; i < N; i++){
-				for (int j = 0; j < N; j++){
-					if (i == 0){
-						down = i;
+			for (int i = 0; i < N/4; i++){
+				for (int j = 0; j < N/4; j++){
+					if (i*4+0 == 0){
+						down = i*4+0;
 					}
 					else{
-						down = i-1;
+						down = i*4+0-1;
 					}
-					if (i == N-1){
-						up = i;
+					if (i*4+0 == N-1){
+						up = i*4+0;
 					}
 					else{
-						up = i+1;
+						up = i*4+0+1;
 					}
 
-					if (j == 0){
-						left = j;
+					if (j*4+0 == 0){
+						left = j*4+0;
 					}
 					else{
-						left = j-1;
+						left = j*4+0-1;
 					}
 
-					if (j == N-1){
-						right = j;
+					if (j*4+0 == N-1){
+						right = j*4+0;
 					}
 					else{
-						right = j+1;
+						right = j*4+0+1;
 					}
-				lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
-				lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
-				du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
-				dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);
+				lapu = u[up][j*4+0] + u[down][j*4+0] + u[i*4+0][left] + u[i*4+0][right] + -4.0*u[i*4+0][j*4+0];
+				lapv = v[up][j*4+0] + v[down][j*4+0] + v[i*4+0][left] + v[i*4+0][right] + -4.0*v[i*4+0][j*4+0];
+				du[i*4+0][j*4+0] = DD*lapu + u[i*4+0][j*4+0]*(1.0 - u[i*4+0][j*4+0])*(u[i*4+0][j*4+0]-b) - v[i*4+0][j*4+0];
+				dv[i*4+0][j*4+0] = d*DD*lapv + c*(a*u[i*4+0][j*4+0] - v[i*4+0][j*4+0]);
+
+				if (i*4+1 == 0){
+					down = i*4+1;
+				}
+				else{
+					down = i*4+1-1;
+				}
+				if (i*4+1 == N-1){
+					up = i*4+1;
+				}
+				else{
+					up = i*4+1+1;
+				}
+
+				if (j*4+1 == 0){
+					left = j*4+1;
+				}
+				else{
+					left = j*4+1-1;
+				}
+
+				if (j*4+1 == N-1){
+					right = j*4+1;
+				}
+				else{
+					right = j*4+1+1;
+				}
+				lapu = u[up][j*4+1] + u[down][j*4+1] + u[i*4+1][left] + u[i*4+1][right] + -4.0*u[i*4+1][j*4+1];
+				lapv = v[up][j*4+1] + v[down][j*4+1] + v[i*4+1][left] + v[i*4+1][right] + -4.0*v[i*4+1][j*4+1];
+				du[i*4+1][j*4+1] = DD*lapu + u[i*4+1][j*4+1]*(1.0 - u[i*4+1][j*4+1])*(u[i*4+1][j*4+1]-b) - v[i*4+1][j*4+1];
+				dv[i*4+1][j*4+1] = d*DD*lapv + c*(a*u[i*4+1][j*4+1] - v[i*4+1][j*4+1]);
+
+				if (i*4+2 == 0){
+					down = i*4+2;
+				}
+				else{
+					down = i*4+2-1;
+				}
+				if (i*4+2 == N-1){
+					up = i*4+2;
+				}
+				else{
+					up = i*4+2+1;
+				}
+
+				if (j*4+2 == 0){
+					left = j*4+2;
+				}
+				else{
+					left = j*4+2-1;
+				}
+
+				if (j*4+2 == N-1){
+					right = j*4+2;
+				}
+				else{
+					right = j*4+2+1;
+				}
+				lapu = u[up][j*4+2] + u[down][j*4+2] + u[i*4+2][left] + u[i*4+2][right] + -4.0*u[i*4+2][j*4+2];
+				lapv = v[up][j*4+2] + v[down][j*4+2] + v[i*4+2][left] + v[i*4+2][right] + -4.0*v[i*4+2][j*4+2];
+				du[i*4+2][j*4+2] = DD*lapu + u[i*4+2][j*4+2]*(1.0 - u[i*4+2][j*4+2])*(u[i*4+2][j*4+2]-b) - v[i*4+2][j*4+2];
+				dv[i*4+2][j*4+2] = d*DD*lapv + c*(a*u[i*4+2][j*4+2] - v[i*4+2][j*4+2]);
+
+
+				if (i*4+3 == 0){
+					down = i*4+3;
+				}
+				else{
+					down = i*4+3-1;
+				}
+				if (i*4+3 == N-1){
+					up = i*4+3;
+				}
+				else{
+					up = i*4+3+1;
+				}
+
+				if (j*4+3 == 0){
+					left = j*4+3;
+				}
+				else{
+					left = j*4+3-1;
+				}
+
+				if (j*4+3 == N-1){
+					right = j*4+3;
+				}
+				else{
+					right = j*4+3+1;
+				}
+				lapu = u[up][j*4+3] + u[down][j*4+3] + u[i*4+3][left] + u[i*4+3][right] + -4.0*u[i*4+3][j*4+3];
+				lapv = v[up][j*4+3] + v[down][j*4+3] + v[i*4+3][left] + v[i*4+3][right] + -4.0*v[i*4+3][j*4+3];
+				du[i*4+3][j*4+3] = DD*lapu + u[i*4+3][j*4+3]*(1.0 - u[i*4+3][j*4+3])*(u[i*4+3][j*4+3]-b) - v[i*4+3][j*4+3];
+				dv[i*4+3][j*4+3] = d*DD*lapv + c*(a*u[i*4+3][j*4+3] - v[i*4+3][j*4+3]);
+
+				// 	if (i == 0){
+				// 		down = i;
+				// 	}
+				// 	else{
+				// 		down = i-1;
+				// 	}
+				// 	if (i == N-1){
+				// 		up = i;
+				// 	}
+				// 	else{
+				// 		up = i+1;
+				// 	}
+
+				// 	if (j == 0){
+				// 		left = j;
+				// 	}
+				// 	else{
+				// 		left = j-1;
+				// 	}
+
+				// 	if (j == N-1){
+				// 		right = j;
+				// 	}
+				// 	else{
+				// 		right = j+1;
+				// 	}
+				// lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
+				// lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
+				// du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
+				// dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);
 				}
 			}		
 	}
