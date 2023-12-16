3f2a4d520d1a7ec65fc6b3365fe59eb8f6f2b2d1
re8423 <maina8423@gmail.com>
2023-12-16 03:23:57 +0000

    bash

--- part1.c
+++ part1.c
@@ -16,61 +16,65 @@ void init(double u[N][N], double v[N][N]){
 }
 
 void dxdt(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){ // u,v are not being changed (no need for reduction)
-	
+	double lapu, lapv;
+	int up, down, left, right;
 	// #pragma omp parrallel for schedule(static, 128)
 
-	#pragma omp parrallel shared
+	#pragma omp parrallel
 	{
 		#pragma omp single
 		{
-			double lapu, lapv;
-			int up, down, left, right;
+
 			for (int i = 0; i < N; i++){
 				for (int j = 0; j < N; j++){
-					#pragma omp task
-					{
-						if (i == 0){
-							down = i;
-						}
-						else{
-							down = i-1;
-						}
-					}
-					#pragma omp task
-					{
-						if (i == N-1){
-							up = i;
-						}
-						else{
-							up = i+1;
-						}
-					}
-					#pragma omp task
-					{
-						if (j == 0){
-							left = j;
-						}
-						else{
-							left = j-1;
-						}
-					}
-					#pragma omp task
-					{
-						if (j == N-1){
-							right = j;
-						}
-						else{
-							right = j+1;
-						}
-					}
-					#pragma omp taskwait
 
 					#pragma omp task
 					{
-						lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
-						lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
-						du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
-						dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);
+						#pragma omp task
+						{
+							if (i == 0){
+								down = i;
+							}
+							else{
+								down = i-1;
+							}
+						}
+						#pragma omp task
+						{
+							if (i == N-1){
+								up = i;
+							}
+							else{
+								up = i+1;
+							}
+						}
+						#pragma omp task
+						{
+							if (j == 0){
+								left = j;
+							}
+							else{
+								left = j-1;
+							}
+						}
+						#pragma omp task
+						{
+							if (j == N-1){
+								right = j;
+							}
+							else{
+								right = j+1;
+							}
+						}
+						#pragma omp taskwait
+
+						#pragma omp task
+						{
+							lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
+							lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
+							du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
+							dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);
+						}
 					}
 					
 				
