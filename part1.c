ca4851705fa177468318c77297f185f7fd1ce0a9
re8423 <maina8423@gmail.com>
2023-12-16 04:43:57 +0000

    bash

--- part1.c
+++ part1.c
@@ -7,10 +7,10 @@ void init(double u[N][N], double v[N][N]){
 	double uhi, ulo, vhi, vlo;
 	uhi = 0.5; ulo = -0.5; vhi = 0.1; vlo = -0.1; // these are shared vars
 
-
+	//collapse wouldnt make difference if using 128 threads
 	#pragma omp parrallel //128 is grain size shared(u, v) default(shared) schedule(static, 128)
 	{
-		#pragma omp for
+		#pragma omp for schedule(static, 128)
 			for (int i=0; i < N; i++){ //vars declared in loop are private
 				for (int j=0; j < N; j++){
 					u[i][j] = ulo + (uhi-ulo)*0.5*(1.0 + tanh((i-N/2)/16.0)); 
@@ -28,7 +28,7 @@ void dxdt(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){ // 
 
 	#pragma omp parrallel
 	{
-		#pragma omp for
+		#pragma omp for schedule(static, 128)
 			for (int i = 0; i < N; i++){
 				for (int j = 0; j < N; j++){
 					if (i == 0){
