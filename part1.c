5168026d34d7f53b0b36a089619019186f94e7ae
re8423 <maina8423@gmail.com>
2023-12-16 17:36:27 +0000

    bash

--- part1.c
+++ part1.c
@@ -76,9 +76,8 @@ void step(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){
 	// 		}
 	// }
 
-	#pragma omp parallel 
 	for (int i = 0; i < N; i++){
-		#pragma omp for nowait
+		#pragma omp parallel  for
 		for (int j = 0; j < N; j++){
 			u[i][j] += dt*du[i][j];
 			v[i][j] += dt*dv[i][j];
