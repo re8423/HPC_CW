6e824a75df6af83ac0bf0d3f4a66967c285438b0
re8423 <maina8423@gmail.com>
2023-12-16 09:01:56 +0000

    bash

--- part1.c
+++ part1.c
@@ -13,10 +13,10 @@ void init(double u[N][N], double v[N][N]){
 		#pragma omp for schedule(dynamic) 
 			for (int i=0; i < N; i++){ //vars declared in loop are private
 				for (int j=0; j < N; j++){
-					// u[i][j] = ulo + (uhi-ulo)*0.5*(1.0 + tanh((i-N/2)/16.0)); 
-					// v[i][j] = vlo + (vhi-vlo)*0.5*(1.0 + tanh((j-N/2)/16.0));
-					u[i][j] = ulo + (uhi-ulo)*0.5*(1.0 + tanh((i-N/2)*0.0625)); 
-					v[i][j] = vlo + (vhi-vlo)*0.5*(1.0 + tanh((j-N/2)*0.0625));
+					u[i][j] = ulo + (uhi-ulo)*0.5*(1.0 + tanh((i-N/2)/16.0)); 
+					v[i][j] = vlo + (vhi-vlo)*0.5*(1.0 + tanh((j-N/2)/16.0));
+					// u[i][j] = ulo + (uhi-ulo)*0.5*(1.0 + tanh((i-N/2)*0.0625)); 
+					// v[i][j] = vlo + (vhi-vlo)*0.5*(1.0 + tanh((j-N/2)*0.0625));
 				}
 			}
 	}
