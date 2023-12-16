db2f6ac4ffba87701f96a206d31a9311dc5d0fb4
re8423 <maina8423@gmail.com>
2023-12-16 03:26:57 +0000

    bash

--- part1.c
+++ part1.c
@@ -27,7 +27,7 @@ void dxdt(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){ // 
 
 			for (int i = 0; i < N; i++){
 				for (int j = 0; j < N; j++){
-
+					
 					#pragma omp task
 					{
 						#pragma omp task
@@ -67,14 +67,13 @@ void dxdt(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){ // 
 							}
 						}
 						#pragma omp taskwait
-
-						#pragma omp task
-						{
-							lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
-							lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
-							du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
-							dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);
-						}
+					}
+					#pragma omp task
+					{
+						lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
+						lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
+						du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
+						dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);
 					}
 					
 				
