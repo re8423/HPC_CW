b03cd2d28bfd5e0a4cd13fe5d60157d96f05da25
re8423 <maina8423@gmail.com>
2023-12-17 20:26:17 +0000

    bash

--- part1_test3.c
+++ part1_test3.c
@@ -64,7 +64,7 @@ void step(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){
 }
 
 double norm(double x[N][N], double nrmx){
-	
+	nrmx = 0.0;
 	#pragma omp for 
 	for (int i = 0; i < N; i++){
 		for (int j = 0; j < N; j++){
@@ -97,8 +97,7 @@ int main(int argc, char** argv){
 		if (k%m == 0){
 			// calculate the norms
 			nrmu = norm(u, nrmx);
-			nrmx = 0.0;
-			nrmv = norm(v), nrmx;
+			nrmv = norm(v, nrmx);
 			printf("t = %2.1f\tu-norm = %2.5f\tv-norm = %2.5f\n", t, nrmu, nrmv);
 			fprintf(fptr, "%f\t%f\t%f\n", t, nrmu, nrmv);
 		}
