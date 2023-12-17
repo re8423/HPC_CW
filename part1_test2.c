1cd223c031c7fb62617a0f97beff7a21f723002f
re8423 <maina8423@gmail.com>
2023-12-17 19:19:28 +0000

    bash

--- part1_test2.c
+++ part1_test2.c
@@ -23,14 +23,25 @@ void funcB( int a[N][N], int b, int c[N][N] ) {
     }
 }
 
-void funcC( int a[N][N], int b, int c[N][N] ) {
-    #pragma omp for schedule( static )
-    for (int ii = 0; ii < b; ii++) {
-        for (int jj = 0; jj < b; jj++) {
-          a[ii][jj] += 1;
-          c[ii][jj] += 1;
-        }
+// void funcC( int a[N][N], int b, int c[N][N] ) {
+//     #pragma omp for schedule( static )
+//     for (int ii = 0; ii < b; ii++) {
+//         for (int jj = 0; jj < b; jj++) {
+//           a[ii][jj] += 1;
+//           c[ii][jj] += 1;
+//         }
+//     }
+// }
+double funcC (int a[N][N], int b, int c[N][N]){
+    double k = 0;
+    #pragma omp parallel for shared(a,b,c) reduction(+:k)
+    for (int ii = 0; ii < b; ii++){
+        for (int jj = 0; jj < b; jj++){
+          // alter values of a and c
+            k += a[ii][jj]*c[ii][jj];
     }
+    }
+    return k;
 }
 
 int omp_thread_count() {
@@ -52,10 +63,10 @@ printf("This program uses %d threads.\n", omp_thread_count());
 for (int i = 0; i < M; i++){
     funcA(a,b,c);
     funcB(a,b,c);
-    funcC(a,b,c);
+    ans = funcC(a,b,c);
     if (i%m == 0){
         funcC(a,b,c);
-        printf("%d\n", i);
+        printf("%d\t%f\n",i, ans);
     }
 }
 // printf("%d", ans);
