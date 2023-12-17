5f285b68929fc785d2b2d827643f66a5dbe905a9
re8423 <maina8423@gmail.com>
2023-12-17 18:43:53 +0000

    bash

--- part1_test.c
+++ part1_test.c
@@ -57,14 +57,17 @@ void init(double u[N][N], double v[N][N]){
 
 int main(int argc, char** argv){
 
+// double t = 0.0, nrmu, nrmv;
+// double u[N][N], v[N][N], du[N][N], dv[N][N];
+
 int u[N][N], v[N][N];
 int b = N;
 double ans = 0;
-
+init(u, v);
 
 
 #pragma omp parallel shared( u, b, v )
-for (int i = 0; i < M; i++){
+for (int k = 0; k < M; k++){
     funcA(u,b,v);
     funcB(u,b,v);
     
