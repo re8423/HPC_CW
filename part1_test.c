f4eadd5c942120713d50ed3a34fda9f31c4cf02a
re8423 <maina8423@gmail.com>
2023-12-17 18:19:38 +0000

    bash

--- part1_test.c
+++ part1_test.c
@@ -3,7 +3,7 @@
 #include "params.h"				// model & simulation parameters
 #include <omp.h> //openmp header file
 
-void funcA( int* a, int b, int* c ) {
+void funcA( int a, int b, int c ) {
     #pragma omp for schedule( static )
     for (int ii = 0; ii < b; ii++) {
         for (int jj = 0; jj < b; jj++) {
@@ -13,7 +13,7 @@ void funcA( int* a, int b, int* c ) {
     }
 }
 
-void funcB( int* a, int b, int* c ) {
+void funcB( int a, int b, int c ) {
     #pragma omp for schedule( static )
     for (int ii = 0; ii < b; ii++) {
         for (int jj = 0; jj < b; jj++) {
@@ -24,7 +24,7 @@ void funcB( int* a, int b, int* c ) {
 }
 
 
-double funcC (int* a, int b, int* c){
+double funcC (int a, int b, int c){
     double k = 0;
     #pragma omp parallel for shared(a,b,c) reduction(+:k)
     for (int ii = 0; ii < b; ii++){
