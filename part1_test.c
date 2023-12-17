cce8560448d79cf6b2823374288883a773112fbe
re8423 <maina8423@gmail.com>
2023-12-17 18:19:03 +0000

    bash

--- part1_test.c
+++ part1_test.c
@@ -30,7 +30,8 @@ double funcC (int* a, int b, int* c){
     for (int ii = 0; ii < b; ii++){
         for (int jj = 0; jj < b; jj++){
           // alter values of a and c
-            k += a[i][j]*c[i][j];
+            k += a[ii][jj]*c[ii][jj];
+    }
     }
     return k;
 }
@@ -38,7 +39,7 @@ double funcC (int* a, int b, int* c){
 
 int main(int argc, char** argv){
 
-int a[N][N], c[N][N],
+int a[N][N], c[N][N];
 int b = N;
 double ans = 0;
 
