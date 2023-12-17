2a983077c883677728ec4b21df7e0fa4d6b701b7
re8423 <maina8423@gmail.com>
2023-12-17 19:27:18 +0000

    bash

--- part1_test2.c
+++ part1_test2.c
@@ -25,16 +25,6 @@ void funcA( double u[N][N], int b, double v[N][N] ) {
     #pragma omp for schedule( static )
     for (int ii = 0; ii < b; ii++) {
         for (int jj = 0; jj < b; jj++) {
-          u[ii][jj] += 1;
-          v[ii][jj] += 1;
-        }
-    }
-}
-
-void funcB( double u[N][N], int b, double v[N][N] ) {
-    #pragma omp for schedule( static )
-    for (int i = 0; i < b; i++) {
-        for (int j = 0; j < b; j++) {
             if (i == 0){
                 down = i;
             }
@@ -61,10 +51,20 @@ void funcB( double u[N][N], int b, double v[N][N] ) {
             else{
                 right = j+1;
             }
-            lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
-            lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
-            du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
-            dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);	
+        lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] + -4.0*u[i][j];
+        lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
+        du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
+        dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);	
+        }
+    }
+}
+
+void funcB( double u[N][N], int b, double v[N][N] ) {
+    #pragma omp for schedule( static )
+    for (int i = 0; i < b; i++) {
+        for (int j = 0; j < b; j++) {
+          u[ii][jj] += 1;
+          v[ii][jj] += 1;
         }
     }
 }
