b1817453d9e04de620d382e292e2789a57a52b56
re8423 <maina8423@gmail.com>
2023-12-17 20:39:54 +0000

    bash

--- part1_test3.c
+++ part1_test3.c
@@ -63,8 +63,9 @@ void step(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){
 	}
 }
 
+double nrmx = 0.0;
+
 double norm(double x[N][N]){
-	extern double nrmx;
 	double nrmx_temp = 0.0;
 
 	#pragma omp for schedule( static )//reduction(+:nrmx)
@@ -88,7 +89,6 @@ int main(int argc, char** argv){
 	
 	// initialize the state
 	init(u, v);
-	double nrmx = 0.0;
 	// time-loop
 	#pragma omp parallel shared( u, v , du, dv, nrmx) //have to call reduction here but cant pass this to norm since cant change header file
 	for (int k=0; k < M; k++){
