b41146f279f9b112793e0259d332d5e51c21f1fa
re8423 <maina8423@gmail.com>
2023-12-17 20:52:15 +0000

    bash

--- part1_test3.c
+++ part1_test3.c
@@ -65,22 +65,22 @@ void step(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){
 double nrmx = 0.0;
 double norm(double x[N][N]){
 	nrmx = 0.0;
-	#pragma omp for schedule(static)
-	for (int i = 0; i < N; i++){
-		for (int j = 0; j < N; j++){
-			nrmx += x[i][j]*x[i][j];
-		}
-	}
-	// double nrmx_temp = 0.0;
-
-	// #pragma omp for schedule( static )//reduction(+:nrmx)
+	// #pragma omp for schedule(static)
 	// for (int i = 0; i < N; i++){
 	// 	for (int j = 0; j < N; j++){
-	// 		nrmx_temp += x[i][j]*x[i][j];
+	// 		nrmx += x[i][j]*x[i][j];
 	// 	}
 	// }
-	// #pragma omp atomic
-	// nrmx += nrmx_temp;
+	double nrmx_temp = 0.0;
+
+	#pragma omp for schedule( static )//reduction(+:nrmx)
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			nrmx_temp += x[i][j]*x[i][j];
+		}
+	}
+	#pragma omp atomic
+	nrmx += nrmx_temp;
 	return nrmx;
 }
 
@@ -95,7 +95,7 @@ int main(int argc, char** argv){
 	// initialize the state
 	init(u, v);
 	// time-loop
-	#pragma omp parallel shared( u, v , du, dv) reduction(+:nrmx)//have to call reduction here but cant pass this to norm since cant change header file
+	#pragma omp parallel shared( u, v , du, dv) //have to call reduction here but cant pass this to norm since cant change header file
 	// double nrmx = 0.0;
 	for (int k=0; k < M; k++){
 		// track the time
