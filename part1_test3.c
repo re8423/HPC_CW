e026e552a81e78d0dd829777eeafa287fcd91f20
re8423 <maina8423@gmail.com>
2023-12-17 20:33:10 +0000

    bash

--- part1_test3.c
+++ part1_test3.c
@@ -65,9 +65,9 @@ void step(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){
 
 double norm(double x[N][N]){
 	double nrmx = 0.0;
-	double nrmx_temp = 0.0
+	double nrmx_temp = 0.0;
 
-	#pragma omp for //reduction(+:nrmx)
+	#pragma omp for schedule( static )//reduction(+:nrmx)
 	for (int i = 0; i < N; i++){
 		for (int j = 0; j < N; j++){
 			nrmx_temp += x[i][j]*x[i][j];
