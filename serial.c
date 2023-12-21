0a727eeafeb3e7e3f4e91ac30bf6c2afdd697050
re8423 <maina8423@gmail.com>
2023-12-21 03:34:11 +0000

    bash

--- serial.c
+++ serial.c
@@ -61,9 +61,9 @@ void step(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){
 		for (int j = 0; j < N; j++){
 			u[i][j] += dt*du[i][j];
 			v[i][j] += dt*dv[i][j];
-			// printf("%f\n", u[i][j]);
-			// printf("%f\n", v[i][j]);
-			// exit(1);
+			printf("%f\n", u[i][j]);
+			printf("%f\n", v[i][j]);
+			exit(1);
 		}
 	}
 }
