d23b2afc3ec6af226916f42152990cd1af20a69d
re8423 <maina8423@gmail.com>
2023-12-21 02:49:30 +0000

    bash

--- serial.c
+++ serial.c
@@ -46,6 +46,11 @@ void dxdt(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]){
 			lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] + -4.0*v[i][j];
 			du[i][j] = DD*lapu + u[i][j]*(1.0 - u[i][j])*(u[i][j]-b) - v[i][j];
 			dv[i][j] = d*DD*lapv + c*(a*u[i][j] - v[i][j]);
+			printf("%f\n", lapu);
+			printf("%f\n", lapv);
+			printf("%f\n", du[i][j]);
+			printf("%f\n", dv[i][j]);
+			exit();
 		}
 	}
 }
